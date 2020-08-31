a04e5094c79a2bef44f1a142444be32ac6d16c09
Jerry Sewell <jerry.sewell@gmail.com>
2020-08-30 23:42:21 -0700

    test build

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Start it up!');
+  console.log('Start it up!!!!');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
